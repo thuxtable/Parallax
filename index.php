1f5483cef7f49742d69b9566ba1995819fffac34
Tyler <tshuxtable@gmail.com>
2014-10-21 15:17:54 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Parallax Test</title>
+<link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+  <div class="parallax">
+    <div class="parallax__layer parallax__layer--back">
+      <div class="title"><img src="parallax1.jpg" /></div>
+    </div>
+    <div class="parallax__layer parallax__layer--base">
+      <div class="title">This is the foreground</div>
+    </div>
+  </div>
+</body>
+</html>
